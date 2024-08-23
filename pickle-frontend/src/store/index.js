4e292b4c242cc8c1d8e218a70247865855c1ab6e
kim nahyeon <stome6322@inha.edu>
2024-08-23 09:56:31 +0900

    feat: userReducer 예시 코드 작성

---
+++ pickle-frontend/src/store/index.js
@@ -0,0 +1,18 @@
+import { combineReducers } from "redux";
+import {configureStore} from "@reduxjs/toolkit";
+import userReducer from "./reducers/user";
+
+export const rootReducer = combineReducers({
+    user: userReducer,
+});
+
+const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => {
+        const middlewares = getDefaultMiddleware();
+        // middlewares.push(myMiddleware); //커스텀 미들웨어 적용 시
+        return middlewares;
+      },
+});
+
+export default store;
---
+++ pickle-frontend/src/store/reducers/user.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    id: null,
+    name: ""
+};
+
+const userSlice = createSlice({
+    name: "user",
+    initialState: initialState,
+    reducers: {
+        setUser: (state, action) => {
+            //로그인 했을 때 로직 예시
+            state.id = action.payload.user.id;
+            state.name = action.payload.user.name;
+        },
+        logoutUser: (state) => {
+            //로그아웃
+            state.id = null;
+            state.name = "";
+        },
+    }
+});
+
+export const {
+    setUser, logoutUser
+} = userSlice.actions;
+
+export default userSlice.reducer;
