482ac837ad89900a4d69f838190fa45598437bc6
kim nahyeon <stome6322@inha.edu>
2024-08-26 16:21:11 +0900

    feat: 원형 그래프 생성, 더미데이터 입력

---
+++ pickle-frontend/src/components/common/circular-graph/Circular.jsx
@@ -0,0 +1,10 @@
+import { CircularGraph } from './CircularGraph'
+import { StyledGraphDiv } from './CircularGraph.style';
+
+export default function Circular(props) {
+  return (
+    <StyledGraphDiv width={props.width} height={props.height}>
+        <CircularGraph data={props.data} />
+    </StyledGraphDiv>
+  )
+}
--- pickle-frontend/src/routes/test/TestPage.jsx
+++ pickle-frontend/src/routes/test/TestPage.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
 import Header from '../../components/common/header/Header'
+import Circular from '../../components/common/circular-graph/Circular'
+
+let data = [
+  {
+    "id": "국내주식",
+    "label": "국내주식",
+    "value": 40,
+    "color": "#FF6767"
+  },
+  {
+    "id": "해외주식",
+    "label": "해외주식",
+    "value": 100,
+    "color": "#FFC27B"
+  },
+  {
+    "id": "채권",
+    "label": "채권",
+    "value": 234,
+    "color": "#FF8B67"
+  },
+  {
+    "id": "ETF",
+    "label": "ETF",
+    "value": 404,
+    "color": "#FFADB6"
+  }
+];
 
 export default function TestPage() {
     return (
     <Header></Header>
+    <Circular width="400px" height="400px" data={data}/>
     )
   }
