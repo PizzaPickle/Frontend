a299464eed32295190b55a4e2a4809e05a7ed852
kim nahyeon <stome6322@inha.edu>
2024-08-26 16:24:37 +0900

    style: prettier 적용

--- pickle-frontend/src/routes/test/TestPage.jsx
+++ pickle-frontend/src/routes/test/TestPage.jsx
@@ -1,39 +1,39 @@
-import React from 'react'
-import Header from '../../components/common/header/Header'
-import Circular from '../../components/common/circular-graph/Circular'
+import React from "react";
+import Header from "../../components/common/header/Header";
+import Circular from "../../components/common/circular-graph/Circular";
 
 let data = [
   {
-    "id": "국내주식",
-    "label": "국내주식",
-    "value": 40,
-    "color": "#FF6767"
+    id: "국내주식",
+    label: "국내주식",
+    value: 40,
+    color: "#FF6767",
   },
   {
-    "id": "해외주식",
-    "label": "해외주식",
-    "value": 100,
-    "color": "#FFC27B"
+    id: "해외주식",
+    label: "해외주식",
+    value: 100,
+    color: "#FFC27B",
   },
   {
-    "id": "채권",
-    "label": "채권",
-    "value": 234,
-    "color": "#FF8B67"
+    id: "채권",
+    label: "채권",
+    value: 234,
+    color: "#FF8B67",
   },
   {
-    "id": "ETF",
-    "label": "ETF",
-    "value": 404,
-    "color": "#FFADB6"
-  }
+    id: "ETF",
+    label: "ETF",
+    value: 404,
+    color: "#FFADB6",
+  },
 ];
 
 export default function TestPage() {
-    return (
-      <>
-    <Header></Header>
-    <Circular width="400px" height="400px" data={data}/>
-      </>
-    )
-  }
+  return (
+    <>
+      <Header></Header>
+      <Circular width="300px" height="300px" data={data} />
+    </>
+  );
+}
