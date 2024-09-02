01d7defa09012565fbca85cfc754c01019da5818
gaeon <rkdjs1104@ajou.ac.kr>
2024-09-02 09:08:42 +0900

    ✨feat: 실시간 시세 서버 코드 수정
    각각의 종목코드별로 데이터를 받을 수 있도록 수정

--- pickle-frontend/src/routes/realtime/RealtimePrice.jsx
+++ pickle-frontend/src/routes/realtime/RealtimePrice.jsx
@@ -1,20 +1,38 @@
-export default function RealtimePrice() {
-    let ws;
-    ws = new WebSocket('ws://3.34.126.55:8080');
-    ws.onopen = function() {
-        console.log('WebSocket 연결이 열렸습니다.');
-        ws.send(JSON.stringify({ id: "005930" }));
-    };
-    ws.onmessage = function(event) {
-        const data = JSON.parse(event.data);
-        console.log("받은 데이터!!", data);
-    };
+import React, { useEffect, useState } from 'react';
 
-    ws.onclose = function(event) {
-        console.log('WebSocket 연결이 닫혔습니다.');
-    };
+export default function RealtimePrice() {
+    const [connections, setConnections] = useState([]);
+
+    useEffect(() => {
+        const wsConnections = [];
+        const stockIds = ["003230", "005930", "000660"];
+
+        stockIds.forEach(id => {
+            const ws = new WebSocket('ws://localhost:3002');
+            ws.onopen = () => {
+                console.log(`WebSocket 연결이 열렸습니다. ID: ${id}`);
+                ws.send(JSON.stringify({ id }));
+            };
+
+            ws.onmessage = (event) => {
+                const data = JSON.parse(event.data);
+                console.log(`받은 데이터!! ID: ${id}`, data);
+            };
+
+            ws.onclose = () => {
+                console.log(`WebSocket 연결이 닫혔습니다. ID: ${id}`);
+                setConnections(prevConnections => prevConnections.filter(conn => conn !== ws));
+            };
+
+            wsConnections.push(ws);
+        });
+        setConnections(wsConnections);
+        return () => {
+            wsConnections.forEach(ws => ws.close());
+        };
+    }, []);
 
     return (
       <div>실시간 시세를 받아오는 페이지</div>
-    )
-  }
+    );
+}
