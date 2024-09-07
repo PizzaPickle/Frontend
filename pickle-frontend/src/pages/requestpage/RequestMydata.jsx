de8b11048886f889bf2e0e3d965146bbe8720329
박찬란 <164446778+colde99@users.noreply.github.com>
2024-09-07 18:10:18 +0900

    feat: 마이데이터 선택 화면 구현

--- pickle-frontend/src/pages/requestpage/RequestMydata.jsx
+++ pickle-frontend/src/pages/requestpage/RequestMydata.jsx
@@ -32,6 +32,174 @@ export default function RequestMydata() {
 
     return new Blob([u8arr], { type: mime });
 }
+    const [slidesHouseData,setSlidesHouseData] = useState([]);
+    const [balanceHouseInfoData, setBalanceHouseInfoData] = useState([]);
+  
+    const [slidesDebtData,setSlidesDebtData] = useState([]);
+    const [balanceDebtInfoData, setBalanceDebtInfoData] = useState([]);
+
+    //부동산 값 포맷
+    function formatCurrency(value) {
+        const isNegative = value < 0;
+        const absoluteValue = Math.abs(value);
+      
+        const billions = Math.floor(absoluteValue / 100000000);
+        const millions = Math.floor((absoluteValue % 100000000) / 10000);
+      
+        const billionPart = billions > 0 ? `${billions}억 ` : '';
+        const millionPart = millions > 0 ? `${millions}만` : '';
+      
+        const formattedValue = `${billionPart}${millionPart}`.trim() || '0원';
+        
+        return isNegative ? `-${formattedValue}` : formattedValue;
+      }
+
+    //부동산 유형 포맷
+    function getHousingType(type) {
+          switch (type) {
+              case '1':
+                  return '토지';
+              case '2':
+                  return '건물';
+              case '3':
+                  return '아파트';
+              case '4':
+                  return '연립';
+              case '5':
+                  return '빌라';
+              default:
+                  return '기타'; // 디폴트값
+          }
+      }
+
+  useEffect(() => {
+    const fetchData = async () => {
+        try {
+            const response = await fetch("/api/mydata", {
+                method: "GET",
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+
+            if (!response.ok) {
+                const errorText = await response.text();
+                console.log(`네트워크 응답이 올바르지 않습니다: ${errorText}`);
+                setError("서버 응답 오류");
+                return;
+            }
+
+            const result = await response.json();
+            console.log(result);
+            
+
+            // 1. 은행
+            setMyBankdata(result.bank);
+            const newSlidesBankData = result.bank.map((elem) => ({
+                bankName: elem.organizationName,
+                accountType: elem.productName,
+                productName: elem.bankProductType,
+                currentBalance: `잔액 ${elem.balanceAmount.toLocaleString()}원`,
+            }));
+            setSlidesBankData(newSlidesBankData);
+
+            const newBalanceBankInfoData = [
+                { label: "총 잔액", amount: result.bankTotalAmount.toLocaleString()+"원"},
+                { label: "총 출금가능액", amount: result.bankWithdrawableAmount.toLocaleString()+"원" },
+            ];
+            setBalanceBankInfoData(newBalanceBankInfoData);
+            console.log(balanceBankInfoData)
+
+
+            // 2. 증권
+            setMySecdata(result.securities);
+            const newSlidesSecData = result.securities.map((elem) =>
+                elem.products.map((prod) => {
+                    const profitAmount = prod.profitAmount;
+                    const purchaseAmount = prod.purchaseAmount;
+
+                    const profitPercentage = (profitAmount / purchaseAmount) * 100;
+                    const formattedProfitPercentage = profitPercentage.toFixed(2);
+
+                    // 조건부 + 기호 추가
+                    const sign = profitPercentage >= 0 ? '+' : '';
+                    const currentBalance = `${sign}${formattedProfitPercentage}%`;
+
+                    return {
+                        bankName: elem.organizationName,
+                        accountType: "",
+                        productName: prod.productName,
+                        currentBalance: currentBalance,
+                    };
+                })
+            ).flat();
+            console.log(newSlidesSecData);
+            setSlidesSecData(newSlidesSecData);
+
+            const newBalanceSecInfoData = [
+                { label: "총 잔액", amount: result.securitiesTotalAmount.toLocaleString()+"원" },
+                { label: "총 출금가능액", amount: result.securitiesTotalPurchaseAmount.toLocaleString()+"원" },
+            ];
+            setBalanceSecInfoData(newBalanceSecInfoData);
+            console.log(balanceSecInfoData)
+
+        
+            // 3. 부동산
+            setMyHousedata(result.housing);
+            const newSlidesHouseData = result.housing.map((elem) => {
+              const profitAmount = elem.profitAmount;
+              const evalAmount = elem.evaluationAmount;
+              console.log(evalAmount)
+              // 조건부 + 기호 추가
+              const sign = profitAmount>= 0 ? '+' : '';
+              const currentBalance = `평가금액 ${formatCurrency(evalAmount)}원`
+              const profitBalance = `(${sign}${formatCurrency(profitAmount)})`;
+
+              return {
+                bankName: elem.address,
+                accountType: getHousingType(elem.housingType),
+                productName: currentBalance,
+                currentBalance: profitBalance
+              }
+              
+            }
+            );
+            console.log(newSlidesHouseData);
+            setSlidesHouseData(newSlidesHouseData);
+
+            const newBalanceHouseInfoData = [
+                { label: "총 가치", amount: result.housingTotalAmount.toLocaleString()+"원" },
+            ];
+            setBalanceHouseInfoData(newBalanceHouseInfoData);
+            console.log(balanceHouseInfoData)    
+            
+
+            // 4. 대출
+            setMyDebtdata(result.debt);
+            const newSlidesDebtData = result.debt.map((elem) => ({
+                bankName: elem.name,
+                accountType: `만기일: ${elem.expireDate}`,
+                productName: `잔여 ${formatCurrency(elem.remainAmount)}원 (총 ${formatCurrency(elem.principalAmount)}원)`,
+                currentBalance: `월 상환액 ${formatCurrency(elem.transAmount)}`,
+            }));
+            setSlidesDebtData(newSlidesDebtData);
+
+            const newBalanceDebtInfoData = [
+                { label: "남은 총액", amount: result.debtRemainAmount.toLocaleString()+"원"},
+                { label: "총 대출금", amount: result.debtTotalAmount.toLocaleString()+"원" },
+            ];
+            setBalanceDebtInfoData(newBalanceDebtInfoData);
+            console.log(balanceDebtInfoData)
+            
+
+        } catch (error) {
+            console.log("데이터 요청 실패:", error.message);
+            setError(error.message);
+        }
+    };
+
+    fetchData();
+}, [token]);
 
     //마이데이터 스크린샷 버튼
     const onClickMydataButton = () => {
