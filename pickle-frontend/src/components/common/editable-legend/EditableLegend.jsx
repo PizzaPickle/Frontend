a41c0efe5394472d55f900c0eb5cb3ca11a73e0d
kim nahyeon <stome6322@inha.edu>
2024-09-08 22:56:46 +0900

    feat: 비율 수정할 수 있는 범례, 종목 테이블 컴포넌트 구현

---
+++ pickle-frontend/src/components/common/editable-legend/EditableLegend.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { useState } from "react";
+import {
+  ColorCircle,
+  LegendDiv,
+  LegendInput,
+  LegendLeftContainer,
+  LegendText,
+} from "./editable-legend.style";
+import { useDispatch, useSelector } from "react-redux";
+import { updateCategoryValueById } from "../../../store/reducers/strategy";
+
+export default function EditableLegend({ category }) {
+  const [value, setValue] = useState(category.value);
+
+  const dispatch = useDispatch();
+  // const category
+
+  const handleTextInput = (e) => {
+    let newValue = e.target.value;
+
+    // 숫자 값을 파싱하여 범위 내에 있는지 확인
+    if (newValue >= 0 && newValue <= 100) {
+      setValue(newValue); // 상태 업데이트
+      dispatch(
+        updateCategoryValueById({
+          categoryId: category.id,
+          newValue: newValue,
+        })
+      );
+    }
+  };
+
+  return (
+    <LegendDiv>
+      <LegendLeftContainer>
+        <ColorCircle color={category.color}></ColorCircle>
+        <LegendText>{category.id}</LegendText>
+      </LegendLeftContainer>
+      <div
+        style={{
+          display: "flex",
+        }}
+      >
+        <LegendInput
+          type="number"
+          min="0"
+          max="100"
+          value={value}
+          onChange={handleTextInput}
+        ></LegendInput>
+        <LegendText>%</LegendText>
+      </div>
+    </LegendDiv>
+  );
+}
