ed9320b9148521f484a129269ae7706c4f0f42fd
gaeon <rkdjs1104@ajou.ac.kr>
2024-09-09 17:12:12 +0900

    ✨feat: 현재가 시세 받아오는 로직 파일 생성

---
+++ pickle-frontend/src/pages/portfolio/fetchStockPrices.js
@@ -0,0 +1,60 @@
+import { setPrices, setLoading, setError } from '../../store/reducers/currentprice'; // Adjust the import path as needed
+
+export const fetchStockPrices = (productList) => async (dispatch) => {
+
+  dispatch(setLoading(true));
+  dispatch(setError(null));
+
+  try {
+    console.log(productList);
+    const requests = productList.map(product => {
+      let apiUrl;
+      let requestCode = product.code;
+
+      switch (product.categoryName) {
+        case '국내':
+          apiUrl = '/api/stock/current-price';
+          break;
+        case '해외':
+          apiUrl = '/api/overseas-stock/current-price';
+          // requestCode = `R${product.code}`;
+          break;
+        case '채권':
+          apiUrl = '/api/bond/current-price';
+          break;
+        case 'ETF':
+          apiUrl = '/api/ETF/current-price';
+          break;
+        case '원자재':
+            apiUrl = '/api/ETF/current-price';
+        break;
+        default:
+          apiUrl = '/api/stock/current-price';
+          break;
+      }
+
+      return fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ stockId: requestCode }),
+      })
+        .then(response => response.json())
+        .then(data => ({ code: product.code, price: data }));
+    });
+
+    const prices = await Promise.all(requests);
+    const pricesMap = prices.reduce((acc, { code, price }) => {
+      acc[code] = price;
+      console.log(acc);
+      return acc;
+    }, {});
+
+    dispatch(setPrices(pricesMap));
+  } catch (error) {
+    dispatch(setError(error.message));
+  } finally {
+    dispatch(setLoading(false));
+  }
+};
