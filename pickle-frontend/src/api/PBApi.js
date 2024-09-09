3ea4bfeae80c2c7a309aab521ebefba80d203496
kim nahyeon <stome6322@inha.edu>
2024-09-09 15:26:27 +0900

    feat: 통합 백테스트 api 연결

--- pickle-frontend/src/api/PBApi.js
+++ pickle-frontend/src/api/PBApi.js
@@ -1,3 +1,28 @@
 /**
  * PB관련된 api 요청 함수 모음
  */
+import { backtestInstance } from "./axiosInstance";
+
+export const integratedBacktest = async (asset_groups) => {
+    try {
+        const response = await backtestInstance.post(
+            '/backtest/integrated',
+            asset_groups
+        )
+        return response.data;
+    } catch (error) {
+        console.log("통합 백테스트 실패", error);
+    }
+}
+
+export const backtest = async (data) => {
+    try {
+        const response = await backtestInstance.post(
+            '/backtest',
+            data
+        )
+        return response.data;
+    } catch (error) {
+        console.log("백테스트 실패", error);
+    }
+}
--- pickle-frontend/src/components/common/backtest/RunBacktestButton.jsx
+++ pickle-frontend/src/components/common/backtest/RunBacktestButton.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 
-const RunBacktestButton = ({ onClick }) => {
+const RunBacktestButton = ({ onClick, onClickIntegrated }) => {
   return (
     <div style={{display: "flex", gap: "10px", paddingBottom: "10px"}}>
       <Button onClick={onClick}>백테스트 진행</Button>
-      <Button variant="secondary">전체 백테스트 진행</Button>
+      <Button onClick={onClickIntegrated} variant="secondary">전체 백테스트 진행</Button>
     </div>
   );
 };
